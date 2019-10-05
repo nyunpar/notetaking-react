5b9e277ebba74b152e74ca118e71da9df6e9a548
Nirwan <ny.unpar@gmail.com>
2019-10-05 23:02:37 +0700

    selesai server graphql

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 import express from "express";
+import graphlHTTP from "express-graphql";
 import mongoose from "mongoose";
+import schema from "./schema";
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/notetaking_db", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify:false
 });
 
 const app = express();
 const PORT = 4300;
 app.get("/", (req, res) => {
   res.json({
-    message: "Notetaking API v1.1"
+    message: "Notetaking API v1"
   });
 });
+
+app.use(
+  "/graphql",
+  graphlHTTP({
+    schema: schema,
+    graphiql: true
+  })
+);
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);
 });
--- schema.js
+++ schema.js
@@ -8,10 +8,26 @@ const typeDefs = `
   date: Date,
   content: String!
  }
+
 scalar Date
+
 type Query {
+  getNote(_id: ID!): Note
   allNotes: [Note]
  }
+ input NoteInput {
+  title: String!
+  content: String!
+ }
+ input NoteUpdateInput {
+  title: String
+  content: String
+}
+type Mutation {
+ createNote(input: NoteInput) : Note
+ updateNote(_id: ID!, input: NoteUpdateInput): Note
+ deleteNote(_id: ID!) : Note
+}
 `;
 
 const schema = makeExecutableSchema({
